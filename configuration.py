300af322b09c48bd093024f585fb9dd0e88285f0
Alexander <goryunoff.al1983@yandex.ru>
2024-03-24 20:06:59 +0300

    automation and code changes

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://ca57068b-3561-4126-be93-6d6c7774f95a.serverhub.praktikum-services.ru"
+URL_SERVICE = "https://ca915c0a-163a-4e84-b371-05f9b3cd1a0b.serverhub.praktikum-services.ru"
 CREATE_ORDER = "/api/v1/orders"
-GET_ORDER_BY_TRACK = "/api/v1/orders/track"
+GET_ORDER_BY_TRACK = "/api/v1/orders/track?t="
--- stand_request.py
+++ stand_request.py
@@ -4,16 +4,9 @@ import configuration
 
 
 def order_create():
+# Создание заказа
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER, json=data.order_create_body)
 
 
-response = order_create()
-track_no = response.json()['track']
-
-
-
-def get_order_info_by_track():
-    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_BY_TRACK, params={'t': track_no})
-#otvet = get_order_info_by_track()
-
-#print(otvet.json())
+def get_order_info_by_track(track):
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_BY_TRACK + track)
